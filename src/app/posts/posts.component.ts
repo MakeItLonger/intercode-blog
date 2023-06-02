67ef07ca9d7140adafeee19a4638cbb475d61307
MakeItLonger <zavadiuk@gmail.com>
2023-06-02 14:30:02 +0300

    refactor display posts from subscribe to async|pipe

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,54 +1,47 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
 import { PageEvent } from '@angular/material/paginator';
 import { QueryRequest } from '../query-request.model';
-import { Subscription, tap } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent implements OnInit, OnDestroy {
+export class PostsComponent {
   posts?: Post[];
   isFetching = false;
   error = null;
   currentPage = 0;
   total?: number;
 
-  filteredPostSubscription!: Subscription;
-
   queryParams: QueryRequest = {};
+
+  posts$ = this.postsService.filteredPost$.pipe(
+    tap((queryRequest) => {
+      Object.assign(this.queryParams, queryRequest);
+    }),
+    switchMap(() => {
+      return this.postsService.getPostsByQueryParams(this.queryParams);
+    }),
+    tap((res) => {
+      this.total = res.total;
+    }),
+    map((response) => {
+      return response.posts;
+    }),
+  );
 
   constructor(private postsService: PostsService) {}
 
-  ngOnInit() {
-    this.isFetching = true;
-
-    this.filteredPostSubscription = this.postsService.filteredPost$.subscribe((queryRequest: QueryRequest) => {
-      this.isFetching = true;
-      Object.assign(this.queryParams, queryRequest);
-      this.makeQueryRequest(this.queryParams);
-    });
-  }
   handlePageEvent(pageEvent: PageEvent) {
     this.currentPage = pageEvent.pageIndex;
-    this.queryParams.page = String(this.currentPage + 1);
-    this.queryParams.limit = String(pageEvent.pageSize);
 
-    this.makeQueryRequest(this.queryParams);
-  }
+    const page = String(this.currentPage + 1);
+    const limit = String(pageEvent.pageSize);
 
-  makeQueryRequest(queryParams: QueryRequest) {
-    this.postsService.getPostsByQueryParams(queryParams).subscribe((res) => {
-      this.isFetching = false;
-      this.posts = res.posts;
-      this.total = res.total;
-    });
-  }
-
-  ngOnDestroy() {
-    this.filteredPostSubscription.unsubscribe();
+    this.postsService.filteredPost$.next({ page, limit });
   }
 }
